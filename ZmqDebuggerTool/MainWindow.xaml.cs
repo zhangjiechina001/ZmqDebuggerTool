23e6ba7dd245788e4f09648e4d3466cb34459081
GS_ZJ <295737015@qq.com>
2023-12-12 17:51:57 +0800

    优化逻辑

--- ZmqDebuggerTool/MainWindow.xaml.cs
+++ ZmqDebuggerTool/MainWindow.xaml.cs
@@ -20,8 +20,6 @@ namespace ZmqDebuggerTool
     /// </summary>
     public partial class MainWindow : Window
     {
-        ZmqSubscriber _sub = new ZmqSubscriber();
-        ZmqRequester _req = new ZmqRequester();
         JsonConfig _configuration;
         public MainWindow(JsonConfig configuration)
         {
@@ -29,41 +27,11 @@ namespace ZmqDebuggerTool
             _configuration = configuration;
         }
 
-        private void SubOnDataReceive(byte[] obj)
-        {
-            string result=Encoding.UTF8.GetString(obj);
-            Dispatcher.Invoke(new Action(() =>
-            {
-                txtLog.AppendText(result);
-                txtLog.AppendText(Environment.NewLine);
-            }));
-        }
-
-        private void btnSub_Click(object sender, RoutedEventArgs e)
-        {
-            _sub.ReInit(cmbAddress.Text);
-        }
-
-        private void btnSetSend_Click(object sender, RoutedEventArgs e)
-        {
-            _req.ReInit(cmbAddress.Text);
-        }
-
-        private void btnSend_Click(object sender, RoutedEventArgs e)
-        {
-            _req.Send(txtSend.Text);
-        }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            _sub.OnDataReceive += SubOnDataReceive;
-            JObject obj = new JObject()
-            {
-                { "theme","measureControl"},
-                { "param","startMeasure"}
-            };
-            txtSend.Text = obj.ToString();
             zmqView.Configuration= _configuration;
+            zmqSubView.Configuration= _configuration;
         }
     }
 }
---
+++ ZmqDebuggerTool/View/ZmqSubscriberView.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZmqDebuggerTool.Communication;
+
+namespace ZmqDebuggerTool.View
+{
+    /// <summary>
+    /// ZmqSubscriberView.xaml 的交互逻辑
+    /// </summary>
+    public partial class ZmqSubscriberView : UserControl
+    {
+        ZmqSubscriber _zmqSubscriber=new ZmqSubscriber();
+
+        public ZmqSubscriberView()
+        {
+            InitializeComponent();
+            _zmqSubscriber.OnDataReceive += ZmqSubscriber_OnDataReceive;
+        }
+
+        private void ZmqSubscriber_OnDataReceive(byte[] obj)
+        {
+            string result = Encoding.UTF8.GetString(obj);
+            Dispatcher.Invoke(new Action(() =>
+            {
+                if(chbSubscriber.IsChecked == false)
+                {
+                    return;
+                }
+
+                txtRec.AppendText(result);
+                txtRec.AppendText(Environment.NewLine);
+                if (txtRec.LineCount > 1000)
+                {
+                    txtRec.Clear();
+                }
+            }));
+        }
+
+        public JsonConfig Configuration { get; set; }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            _zmqSubscriber.ReInit(txtAddress.Text);
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtAddress.Text = Configuration.GetSectionString("SubscriberAddress");
+        }
+
+        private void cmbCmds_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
